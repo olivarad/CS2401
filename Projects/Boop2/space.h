a5972fa805410fe1b2fb821d6f5351717dbc9ad1
olivarad <olivia.radecki02@gmail.com>
2023-04-05 19:23:47 -0400

    minor progress on board disp

--- Projects/Boop2/space.h
+++ Projects/Boop2/space.h
@@ -1,23 +1,25 @@
 #ifndef SPACE_H
 #define SPACE_H
 
-namespace main_savitch_14
-{
-    class Space{
-        public:
-            // Default constructor (Sets space to empty)
-            Space();
+#include <string>
 
-            //Space mutator function (Move format is substring of full move with only piece information)
-            void Space_mutator(std::string& move);
 
-            //Space Accessor
-            int Access_State();
+class Space{
+    public:
+        // Default constructor (Sets space to empty)
+        Space();
 
-        private:
-            // State variable 0 is empty 1 is k1, 2 is k2, 3 is c1, 4 is c2
-            int state;
-    };
-}
+        //Space mutator function (Move format is substring of full move with only piece information)
+        void Space_mutator(std::string& move);
+
+        //Space Accessor
+        int Access_State();
+
+    private:
+        // State variable 0 is empty 1 is k1, 2 is k2, 3 is c1, 4 is c2
+        int state;
+            
+        std::string c1[3] = {"\e[38;5;208m@@@@@@\e[0m"}, {"@@@@@"}, {"\e[38;5;90m@@@@@\e[0m"}; // String array (3 rows , 5 columns) for stroing player 1 cat models
+};
 
 #endif
